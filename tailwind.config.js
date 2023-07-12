1786a63ef6cd7744e1eb5291536defd975ad8af4
Souhail FAZOUANE <fazouane7@gmail.com>
2023-07-12 14:23:48 +0200

    add colors and organise the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,27 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontSize: {
+      'xl': '14px',
+      "base": "13px",
+      "sm": "12px"
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        primary: 'Inter'
       },
+      fontWeight: {
+        bold: 700,
+        semibold: 600,
+        regular: 400
+      },
+      colors: {
+        hintedGrey1: '#0e0e10',
+        hintedGrey2: '#18181b',
+        backgroundButton: '#9147ff',
+        hintedGrey11: '#dedee3',
+        colorMessage: '#efeff1'
+      }
     },
   },
   plugins: [],
